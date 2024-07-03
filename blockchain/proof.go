88ec46fbabf4461e7decbec3860cadca34bfbff5
Zainab Adegoke <zainabolaitan5@gmail.com>
2024-07-03 15:53:40 +0200

    added proof of work

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,1 +1,18 @@
 package blockchain
+
+import (
+	"math/big"
+)
+
+const Difficulty = 10;
+
+type ProofOfWork struct {
+	Block *Block
+	Target *big.Int
+}
+
+func NewProofOfWork(block *Block) *ProofOfWork  {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+	return &ProofOfWork{block, target}
+}
