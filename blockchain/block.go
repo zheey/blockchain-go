4a6a567d1acf1b0a0cbcd074a4d760e8caa841d1
Zainab Adegoke <zainabolaitan5@gmail.com>
2024-07-03 11:33:40 +0200

    created Initialise and create block function

--- blockchain/block.go
+++ blockchain/block.go
@@ -11,21 +11,21 @@ type Block struct {
 	PrevHash string
 }
 
-func (b *Block) ComputeHash ()   {
+func (b *Block) ComputeHash()   {
 	concatenatedData := bytes.Join([][]byte{[]byte(b.Data), []byte(b.PrevHash)}, []byte{})
 	computedHash := md5.Sum(concatenatedData)
 
 	b.Hash = string(computedHash[:])
 }
 
-func CreateBlock (blockData string, prevHash string) *Block {
+func CreateBlock(blockData string, prevHash string) *Block {
 
 	block := &Block{"", blockData, prevHash}
 	block.ComputeHash()
 	return block;
 }
 
-func Genesis () *Block {
+func Genesis() *Block {
 
 	blockGenesis := CreateBlock("hardcode", "")
 	return blockGenesis;
--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,5 +1,20 @@
 package blockchain
 
+
 type BlockChain struct {
 	Blocks []*Block
+}
+
+func  InitBlockChain() *BlockChain {
+	genesis := Genesis();
+	blockChain := &BlockChain{[]*Block{genesis}}
+
+	return blockChain;
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+
+	lastBlock := chain.Blocks[len(chain.Blocks) - 1]
+	newBlock := CreateBlock(data, lastBlock);
+	chain.Blocks = append(chain.Blocks, newBlock)
 }
