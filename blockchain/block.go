8c651138213e8617a72fa0a4c19dc6e45a29d7f3
Zainab Adegoke <zainabolaitan5@gmail.com>
2024-07-03 09:50:14 +0200

    creating block hash and storing the encded value

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,7 +1,20 @@
 package blockchain
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
 type Block struct {
 	Hash string
 	Data string
 	PrevHash string
+}
+
+func (b *Block) ComputeHash ()   {
+	concatenatedData := bytes.Join([][]byte{[]byte(b.Data), []byte(b.PrevHash)}, []byte{})
+	computedHash := sha256.Sum256(concatenatedData)
+
+	b.Hash = string(hex.EncodeToString(computedHash[:]))
 }
