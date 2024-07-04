71017a6ec010b57f1e56eb9ec42ea5f08092ef3a
Zainab Adegoke <zainabolaitan5@gmail.com>
2024-07-04 12:46:40 +0200

    updated pow

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,6 +1,10 @@
 package blockchain
 
 import (
+	"bytes"
+	"crypto/md5"
+	"encoding/binary"
+	"fmt"
 	"math/big"
 )
 
@@ -15,4 +19,61 @@ func NewProofOfWork(block *Block) *ProofOfWork  {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-Difficulty))
 	return &ProofOfWork{block, target}
+}
+
+func (pow *ProofOfWork) ComputeData(nonce int) []byte {
+    data := bytes.Join(
+        [][]byte{
+            []byte(pow.Block.PrevHash),
+            []byte(pow.Block.Data),
+            make([]byte, 8),
+            make([]byte, 8),
+        },
+        []byte{},
+    )
+
+    binary.BigEndian.PutUint64(data[len(data)-16:], uint64(nonce))
+    binary.BigEndian.PutUint64(data[len(data)-8:], uint64(Difficulty))
+    
+    return data
+}
+
+func (pow *ProofOfWork) MineBlock() (int, []byte) {
+    var intHash big.Int
+    var computedHash [16]byte
+
+    nonce := 0
+    
+    for {
+        computedData := pow.ComputeData(nonce)
+        computedHash = md5.Sum(computedData)
+
+        fmt.Printf("\r%x", computedHash)
+
+        intHash.SetBytes(computedHash[:])
+
+        if intHash.Cmp(pow.Target) == -1 {
+            break
+        }
+
+        nonce++
+    }
+    fmt.Println()
+
+    return nonce, computedHash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+    var intHash big.Int
+
+    computedData := pow.ComputeData(pow.Block.Nonce)
+
+    computedHash := md5.Sum(computedData)
+    intHash.SetBytes(computedHash[:])
+
+    if intHash.Cmp(pow.Target) == -1 {
+        return true
+    } else {
+        return false
+    }
 }
--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"blockChain/blockchain"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Blockchain from Scratch")
+	chain := blockchain.InitBlockChain()
+
+    chain.AddBlock("Block 1")
+    chain.AddBlock("Block 2")
+    chain.AddBlock("Block 3")
+
+    for _, block := range chain.Blocks {
+        fmt.Printf("Previous hash: %x\n", block.PrevHash)
+        fmt.Printf("Data in Block: %s\n", block.Data)
+        fmt.Printf("Hash of block: %x\n", block.Hash)
+        fmt.Println()
+    }
 }
