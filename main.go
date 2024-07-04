1c6439da446c8cb45f95263fbf6c355cb56cc12f
Zainab Adegoke <zainabolaitan5@gmail.com>
2024-07-04 17:01:00 +0200

    added transactions ti main

--- main.go
+++ main.go
@@ -7,20 +7,35 @@ import (
 )
 
 func main() {
-   chain := blockchain.InitBlockChain()
+    chain := blockchain.InitBlockChain()
 
-   chain.AddBlock("Block 1")
-   chain.AddBlock("Block 2")
-   chain.AddBlock("Block 3")
+    chain.AddBlock("Block 1", "Alice", []*blockchain.Transaction{
+        {Sender: "Alice", Receiver: "Bob", Amount: 1.5},
+        {Sender: "Alice", Receiver: "Charlie", Amount: 19.5},
+    })
 
-   for _, block := range chain.Blocks {
-      fmt.Printf("Previous hash: %x\n", block.PrevHash)
-      fmt.Printf("Data in Block: %s\n", block.Data)
-      fmt.Printf("Hash of block: %x\n", block.Hash)
+    chain.AddBlock("Block 2", "Bob", []*blockchain.Transaction{
+        {Sender: "Bob", Receiver: "Charlie", Amount: 2.3},
+    })
 
-      pow := blockchain.NewProofOfWork(block)
-      fmt.Printf("IsValidPoW: %s\n", strconv.FormatBool(pow.Validate()))
+    for _, block := range chain.Blocks {
+        fmt.Printf("Previous hash: %x\n", block.PrevHash)
+        fmt.Printf("Data in Block: %s\n", block.Data)
+        fmt.Printf("Hash of block: %x\n", block.Hash)
 
-      fmt.Println()
-   }
+        pow := blockchain.NewProofOfWork(block)
+        fmt.Printf("IsValidPoW: %s\n", strconv.FormatBool(pow.Validate()))
+        fmt.Println()
+
+        fmt.Println("Transactions:")
+
+        for _, tx := range block.Transactions {
+            fmt.Printf("Sender: %s\n", tx.Sender)
+            fmt.Printf("Receiver: %s\n", tx.Receiver)
+            fmt.Printf("Amount: %f\n", tx.Amount)
+            fmt.Printf("Coinbase: %t\n", tx.Coinbase)
+            fmt.Println()
+        }
+        fmt.Println()
+    }
 }
