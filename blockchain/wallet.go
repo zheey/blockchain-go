9eecfdf1e98cb7ceffe8292b3ac945ac6bafde8e
Zainab Adegoke <zainabolaitan5@gmail.com>
2024-07-04 17:35:52 +0200

    added security

--- blockchain/wallet.go
+++ blockchain/wallet.go
@@ -1,1 +1,74 @@
 package blockchain
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"fmt"
+)
+
+type Wallet struct {
+    PrivateKey *rsa.PrivateKey
+    PublicKey  *rsa.PublicKey
+}
+
+func GenerateRSAKeys() (*rsa.PrivateKey, *rsa.PublicKey, error) {
+    privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+    if err != nil {
+        return nil, nil, err
+    }
+
+    publicKey := &privateKey.PublicKey
+
+    return privateKey, publicKey, nil
+}
+
+func NewWallet() (*Wallet, error) {
+    privateKey, publicKey, err := GenerateRSAKeys()
+    
+    if err != nil {
+        return nil, err
+    }
+
+    return &Wallet{
+        PrivateKey: privateKey,
+        PublicKey:  publicKey,
+    }, nil
+}
+
+func (wallet *Wallet) SignTransaction(transaction *Transaction) (string, error) {
+    dataString := fmt.Sprintf("%s%s%f%t", transaction.Sender, transaction.Receiver, transaction.Amount, transaction.Coinbase)
+
+    hashedData := sha256.Sum256([]byte(dataString))
+
+    signature, err := rsa.SignPKCS1v15(rand.Reader, wallet.PrivateKey, crypto.SHA256, hashedData[:])
+
+    if err != nil {
+        return "", err
+    }
+
+    return base64.StdEncoding.EncodeToString(signature), nil
+}
+
+func VerifyTransaction(transaction *Transaction, publicKey *rsa.PublicKey, signature string) error {
+    dataString := fmt.Sprintf("%s%s%f%t", transaction.Sender, transaction.Receiver, transaction.Amount, transaction.Coinbase)
+
+    hashedData := sha256.Sum256([]byte(dataString))
+
+    signatureBytes, err := base64.StdEncoding.DecodeString(signature)
+
+    if err != nil {
+        return err
+    }
+
+    err = rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hashedData[:], signatureBytes)
+
+    if err != nil {
+        return errors.New("Transaction Signature not valid.")
+    }
+    return nil
+}
--- main.go
+++ main.go
@@ -9,24 +9,64 @@ import (
 func main() {
     chain := blockchain.InitBlockChain()
 
-    chain.AddBlock("Block 1", "Alice", []*blockchain.Transaction{
-        {Sender: "Alice", Receiver: "Bob", Amount: 1.5},
-        {Sender: "Alice", Receiver: "Charlie", Amount: 19.5},
-    })
+    // Create a wallet for Alice.
+    aliceWallet, err := blockchain.NewWallet()
+    
+    if err != nil {
+        fmt.Println("Error creating Alice's wallet:", err)
+        return
+    }
+    fmt.Println("Alice's wallet created successfully")
 
-    chain.AddBlock("Block 2", "Bob", []*blockchain.Transaction{
-        {Sender: "Bob", Receiver: "Charlie", Amount: 2.3},
-    })
+    // Create a wallet for Bob.
+    bobWallet, err := blockchain.NewWallet()
+    
+    if err != nil {
+        fmt.Println("Error creating Bob's wallet:", err)
+        return
+    }
+    fmt.Println("Bob's wallet created successfully")
 
+    // Create a transaction from Alice to Bob.
+    tx := &blockchain.Transaction{
+        Sender:   aliceWallet.PublicKey.N.String(),
+        Receiver: bobWallet.PublicKey.N.String(),
+        Amount:   5.0,
+    }
+    fmt.Println("Alice to Bob Transaction created successfully")
+
+    // Sign the transaction with Alice’s wallet.
+    signature, err := aliceWallet.SignTransaction(tx)
+    if err != nil {
+        fmt.Println("Error signing the transaction:", err)
+        return
+    }
+
+    // Verify the transaction using Alice’s wallet, public key, and the signature.
+    err = blockchain.VerifyTransaction(tx, aliceWallet.PublicKey, signature)
+    
+    if err != nil {
+        fmt.Println("Transaction verification failed:", err)
+        return
+    }
+
+    fmt.Println("Transaction Verified Successfully")
+
+    // Add the verified transaction to the blockchain.
+    chain.AddBlock("Block 1", "Alice", []*blockchain.Transaction{tx})
+    fmt.Println()
+
+    // Print the blockchain.
     for _, block := range chain.Blocks {
         fmt.Printf("Previous hash: %x\n", block.PrevHash)
-        fmt.Printf("Data in Block: %s\n", block.Data)
-        fmt.Printf("Hash of block: %x\n", block.Hash)
+        fmt.Printf("Data: %s\n", block.Data)
+        fmt.Printf("Hash: %x\n", block.Hash)
+
 
         pow := blockchain.NewProofOfWork(block)
         fmt.Printf("IsValidPoW: %s\n", strconv.FormatBool(pow.Validate()))
         fmt.Println()
-
+        
         fmt.Println("Transactions:")
 
         for _, tx := range block.Transactions {
